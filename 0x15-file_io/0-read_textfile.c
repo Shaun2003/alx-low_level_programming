4d336d7bdaf03ba33b56f92d3d764697e6f4a576
Shaun2003 <tshimangadzoshaun21@gmail.com>
2023-05-09 00:34:04 -0700

    it was challenging

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,8 +8,7 @@
  * @letters: num of letters to be read
  *
  * Return: w - actual num of bytes read and printed
- * 0 when function fails or filename is 
- *  NULL
+ * 0 when function fails or filename is NULL
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
